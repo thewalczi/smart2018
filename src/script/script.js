f234611c3a8416033087a71a4247bf3cf5794384
thewalczi <walczak.kam@gmail.com>
2018-03-07 23:56:32 +0100

    Made mobile nav

--- src/script/script.js
+++ src/script/script.js
@@ -14,9 +14,7 @@ $(document).ready(function() {
 
     var boxH = $('.be-smart').height();
     var fanH = $('.fan').height();
-    // var $h1 = $(".be-smart");
 
-    // console.log($(window).height()/boxH);
     $('.fan').css('bottom', boxH - fanH);
     $(window).on('scroll load', function() {
         var window_offset = ($(window).scrollTop() * -1 + boxH) * -1;
@@ -25,30 +23,67 @@ $(document).ready(function() {
         } else {
             $('.fan').css('bottom', -5);
         }
-
-
     });
 
 
+    /*ADDING CLASS TO HEADER ON SCROLL*/
+
     $(window).on('load scroll', function() {
-        if ($(window).scrollTop() > 300) {
-            $('header').addClass('fixed');
+
+        var scrollPosition = 0;
+
+        if ($(window).scrollTop() > scrollPosition) {
+            $('.header-bar').addClass('header-bar--scroll');
         } else {
-            $('header').removeClass('fixed');
+            $('.header-bar').removeClass('header-bar--scroll');
         }
     });
 
+    /*CHANGED NAV TO MOBILE-NAV ON RESIZE*/
 
-// $(window).on('load resize', function() {
 
-//     var windowW = $(window).width();
+    $(window).on('load resize', function() {
 
-//     if (windowW < 768) {
-//         $('.nav').addClass('nav-mobile');
-//     } else {
-//         $('.nav').removeClass('nav-mobile');
-//     }
-//     ;
-// });
+        // var menuOpen = 0;
+        // var navList = $('.nav.nav-mobile > ul');
+        var windowW = $(window).outerWidth();
+
+        setTimeout(function() {
+            if (windowW < 768) {
+                $('.nav').addClass('nav-mobile');
+            } else {
+                $('.nav').removeClass('nav-mobile');
+            }
+
+        }, 100)
+
+        setTimeout(function() {
+            $('.nav').css('opacity', '1');
+        }, 300);
+
+
+
+
+        // $('.nav-mobile--btn').click(function() {
+
+        //     if (menuOpen == 0) {
+        //         navList.animate({
+        //             'marginRight': '0px'
+        //         }, 100);
+        //         menuOpen = 1;
+        //     } else if (menuOpen == 1) {
+        //         navList.animate({
+        //             'marginRight': '-250px'
+        //         }, 100)
+        //         menuOpen = 0;
+        //     }
+        //     console.log(menuOpen);
+        // });
+
+    });
+
+    /*SLIDE MENU ON SCROLL*/
+
+
 });
 
